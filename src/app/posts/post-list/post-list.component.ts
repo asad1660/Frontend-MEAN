35902bc2ea354b280a6c559571449b6651fcaeba
asad1660 <asadk1660@gmail.com>
2024-01-17 23:53:34 +0500

    injecting service

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component,Input } from '@angular/core';
 import {Post} from '../post.model'
+import { PostsService } from '../posts.service';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -11,5 +12,10 @@ export class PostListComponent {
 //   {title:"Second Post", content:"This is Second post's content"},
 //   {title:"Third Post", content:"This is Third post's content"}
 // ]
+
 @Input() posts:Post[] =[];
+
+constructor(public postsService :PostsService) {
+  
+ }
 }
