74bc2c0980ec90430604f5bbb0ad3d02d8c73e36
asad1660 <asadk1660@gmail.com>
2024-01-19 18:56:27 +0500

    creating posts static api

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,29 @@ const express = require('express')
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('First Middleware')
-    // next();
-}) 
+// app.use((req,res,next)=>{
+//     console.log('First Middleware')
+//     next();
+// }) 
 
-app.use((req,res,next)=>{
-   res.send('hello from express!')
+app.use('/api/posts',(req,res,next) => {
+   const posts = [
+    {
+        id: "fasd213123",
+        title: "First server side response",
+        content: "This is coming from the server"
+    },
+    {
+        id: "fasd2sada13123",
+        title: "Second server side response",
+        content: "This is coming from the server"
+    }
+   ]
+ return  res.status(200).json(
+    {
+        message:"Posts Fetched Successfully!",
+        posts:posts
+});
 })
 
 module.exports = app;
