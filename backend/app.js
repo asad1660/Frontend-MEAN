61af2b160ad8c03191959feccd44408e292da1b7
asad1660 <asadk1660@gmail.com>
2024-01-19 20:18:24 +0500

    post api with angular integration

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,23 @@
 const express = require('express')
-
+const bodyParser = require('body-parser')
 const app = express();
+app.use(bodyParser.json());
 
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","*")
-    res.setHeader("Access-Control-Allow-Header","Origin, X-Requested-with, Content-Type,Accept")
+    res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-with, Content-Type,Accept")
     res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS,PUT ")
     next();
 }) 
+app.post('/api/posts',(req,res,next) => {
+ 
+  return  res.status(201).json(
+     {
+         message:"Posts added Successfully!",
+ });
+ })
 
-app.use('/api/posts',(req,res,next) => {
+app.get('/api/posts',(req,res,next) => {
    const posts = [
     {
         id: "fasd213123",
