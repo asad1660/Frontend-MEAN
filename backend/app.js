2581270ed8081dce534f7a744121d9f9f6b09911
asad1660 <asadk1660@gmail.com>
2024-01-19 20:03:06 +0500

    Resolving CORS

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,12 @@ const express = require('express')
 
 const app = express();
 
-// app.use((req,res,next)=>{
-//     console.log('First Middleware')
-//     next();
-// }) 
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*")
+    res.setHeader("Access-Control-Allow-Header","Origin, X-Requested-with, Content-Type,Accept")
+    res.setHeader("Access-Control-Allow-Methods","GET, POST, PATCH, DELETE, OPTIONS,PUT ")
+    next();
+}) 
 
 app.use('/api/posts',(req,res,next) => {
    const posts = [
