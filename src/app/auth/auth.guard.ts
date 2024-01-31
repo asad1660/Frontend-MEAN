b1603deff9514cf189101118debbe626ca243cb4
asad1660 <asadk1660@gmail.com>
2024-02-01 00:47:45 +0500

    adding guards on routes

---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,9 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+export function authGuard(authService: AuthService,router:Router): CanActivateFn {
+  if (!authService.getIsAuth()){
+    this.router.navigate(['./'])
+  }
+  return (route, state) => this.authService.getIsAuth();
+}
