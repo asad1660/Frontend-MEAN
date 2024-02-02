22f4c7d481d28e5d4276aabd2b038c94ab1aa864
asad1660 <asadk1660@gmail.com>
2024-02-02 23:54:42 +0500

    implementing authguard with new tecnique

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,9 +1,29 @@
+import { Injectable, inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
+
+@Injectable({
+  providedIn: 'root',
+})
+ export class PermissionsService {
+
+ constructor(
+ private authService: AuthService,
+ public router: Router,
+ ) { }
+
+ canActivate(): any {
+ if (this.authService.getIsAuth()) {
+   return true
+ } else {
+   this.router.navigate(['/']);
+   return false
+   }
+ }
+
+ }
+
 export function authGuard(authService: AuthService,router:Router): CanActivateFn {
-  // if (!authService.getIsAuth()){
-  //   this.router.navigate(['./'])
-  // }
-  return (route, state) => this.authService.getIsAuth();
+  return inject(PermissionsService).canActivate();
 }
