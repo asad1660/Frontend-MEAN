0f5c7314cd6d78eb76270b123590f29f4334c156
asad1660 <asadk1660@gmail.com>
2024-02-02 23:25:08 +0500

    add routing

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -2,8 +2,8 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 export function authGuard(authService: AuthService,router:Router): CanActivateFn {
-  if (!authService.getIsAuth()){
-    this.router.navigate(['./'])
-  }
+  // if (!authService.getIsAuth()){
+  //   this.router.navigate(['./'])
+  // }
   return (route, state) => this.authService.getIsAuth();
 }
